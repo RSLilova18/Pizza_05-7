50a0e782fced7cb6d5241612701fc267bea80025
RSLilova18 <RSLilova18@codingburgas.bg>
2023-02-13 13:56:15 +0200

    Implement class Pizza

--- src/Pizza.java
+++ src/Pizza.java
@@ -4,5 +4,49 @@ public class Pizza {
     private String Topping;
 
 
+    public Pizza (){
+        this.Name = "Margarita";
+        this.Dought = "Bqlo";
+        this.Topping = "Sirene";
+    }
 
+
+    public Pizza(String name, String dought, String topping) {
+        Name = name;
+        Dought = dought;
+        Topping = topping;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDought() {
+        return Dought;
+    }
+
+    public void setDought(String dought) {
+        Dought = dought;
+    }
+
+    public String getTopping() {
+        return Topping;
+    }
+
+    public void setTopping(String topping) {
+        Topping = topping;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "Name='" + Name + '\'' +
+                ", Dought='" + Dought + '\'' +
+                ", Topping='" + Topping + '\'' +
+                '}';
+    }
 }
